dd6792084296bc8cda80e17c6644b342bd9bd9a8
ktaeeun <ktaeeun43@gmail.com>
2023-02-20 10:16:40 +0900

    최종수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { useEffect, useRef, useState } from "react"
-import "./App.css"
+import React, { useState, useRef, useCallback } from 'react'
+import produce from 'immer'
+import { SliderPicker } from 'react-color';
 
-const cols = 30
-const rows = 30
-const randomGrid = () => {
-  const grid = []
-  for (let i = 0; i < rows; i++) {
-    const row = []
-    for (let j = 0; j < cols; j++) {
-      row.push(Math.floor(Math.random() * 2))
-    }
-    grid.push(row)
-  }
-  return grid
-}
-const positions = [
+import './App.css';
+
+const numRows = 70;
+const numCols = 70;
+const delay = 100
+
+
+const operations = [
   [0, 1],
   [0, -1],
   [1, -1],
@@ -22,98 +17,178 @@ const positions = [
   [1, 1],
   [-1, -1],
   [1, 0],
-  [-1, 0],
+  [-1, 0]
 ]
 
-console.log(randomGrid())
-function App() {
-  const [grid, setGrid] = useState()
-  const [start, setStart] = useState(false)
-  const startRef = useRef(start)
-  startRef.current = start
-
-  useEffect(() => {
-    setGrid(randomGrid())
-  }, [])
-
-  function runSimulation() {
-    if (!startRef.current) {
-      return
-    }
-    setGrid((g) => {
-      const next = g.map((row, i) => {
-        return row.map((cell, j) => {
-          let sum = 0
-          positions.forEach((position) => {
-            const x = i + position[0]
-            const y = j + position[1]
-            if (x >= 0 && x < rows && y >= 0 && y < cols) {
-              sum += g[x][y]
-            }
-          })
-          if (sum < 2 || sum > 3) {
-            return 0
-          }
-          if (sum === 3) {
-            return 1
-          }
-          return g[i][j]
-        })
-      })
-      return next
-    })
+function generateEmptyGrid() {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0))
   }
-
-  console.log(grid)
-  return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          spaceBetween: "10px",
-          margin: "2rem",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <button
-          style={{ marginRight: "1rem" }}
-          onClick={() => {
-            setStart(!start)
-            if (!start) {
-              startRef.current = true
-            }
-            setInterval(() => {
-              runSimulation(grid)
-            }, 1000)
-          }}
-        >
-          {start ? "Stop" : "Start"}
-        </button>
-        <button
-          style={{ marginLeft: "1rem" }}
-          onClick={() => setGrid(randomGrid)}
-        >
-          Reset
-        </button>
-      </div>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {grid &&
-          grid.map((rows, i) =>
-            rows.map((col, k) => (
-              <div
-                style={{
-                  width: 30,
-                  height: 30,
-                  backgroundColor: grid[i][k] ? "green" : "",
-                  border: "1px solid black",
-                }}
-              />
-            ))
-          )}
-      </div>
-    </>
-  )
+  return rows
 }
 
-export default App
+function App() {
+  const [running, setRunning] = useState(false)
+  const [color, setColor] = useState("#111")
+  let [counter, setCounter] = useState(0)
+  const [deathMode, setDeathMode] = useState(false)
+  
+
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid()
+  })
+
+  const runningRef = useRef(running)
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+      setGrid(g => {
+        return produce(g, gridCopy => {
+           
+          for (let i = 0; i < numRows; i++) {
+            for (let k = 0; k < numCols; k++) {
+              let neighbors = 0;
+
+              
+              operations.forEach(([x, y]) => {
+                const newI = i + x
+                const newK = k + y
+                
+                if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                  neighbors += g[newI][newK]
+                }
+              })
+
+              
+              
+              if (neighbors < 2 || neighbors > 3) {
+                gridCopy[i][k] = 0;
+              }
+              
+              else if (g[i][k] === 0 && neighbors === 3) {
+                gridCopy[i][k] = 1;
+              }
+              
+            
+            }
+          }
+        })
+      }) 
+    setTimeout(runSimulation, delay)
+  }, [])
+
+  function handleStartSimulation() {
+    setRunning(!running);
+    if(!running) {
+      runningRef.current = true
+      runSimulation()
+    }
+  }
+  
+  
+  function handleNodeClick(i,k) {
+    const newGrid = produce(grid, gridCopy => {
+    gridCopy[i][k] = !grid[i][k] })
+    setGrid(newGrid) 
+  }
+
+  
+  function handleRandomizeGrid() {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => Math.random() > 0.6 ? 1 : 0))
+    }
+    setGrid(rows)
+  }
+
+  
+  function handleDeathMode() {
+      if (counter < 4) {
+        setCounter(counter + 1) 
+        setDeathMode(false)
+      }
+      else if (counter === 4){
+        setDeathMode(true)
+        handleRandomizeGrid()
+        handleStartSimulation()
+        setColor("#EE3633")
+        setCounter(-1) 
+      }
+  }
+
+  
+  return (
+    <div className="page">
+      <div className="title" 
+        onClick={() => handleDeathMode()}
+        style={{
+          color: "#1860B6"
+      }}>
+        <h1>
+          라이프게임
+        </h1>
+      </div>
+
+      <div className="btns">
+        <a  className="startBtn" 
+          style={{
+            backgroundColor: "#1860B6",
+            color: "#fff"
+          }} 
+          onClick={handleStartSimulation}> 
+        {running ? "STOP" : "START"} 
+        </a>
+
+        <a  className="clearBtn" onClick={() => {
+          setGrid(generateEmptyGrid())
+          setRunning(false);
+          if(running) {
+            runningRef.current = false
+          }
+        }}> 
+          삭제
+        </a>
+
+        <a  className="randomizeBtn" onClick={handleRandomizeGrid}>
+          랜덤
+        </a>
+
+        <SliderPicker className="colorPicker"
+          color={color}
+          onChangeComplete={(color) => {
+            setColor(color.hex)
+          }}
+        />
+
+        
+      </div>
+    
+
+      <div className="grid" style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}>
+        {grid.map((rows, i) => 
+          rows.map((col, k) => 
+            <div className="node"
+            key={`${i}-${k}`}
+            onClick={() => handleNodeClick(i,k)}
+            style={{
+              width: 20,
+              height: 20,
+              border: 'solid 1px grey',
+              backgroundColor: grid[i][k] ? color : undefined
+            }} 
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
